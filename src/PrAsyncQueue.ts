a50a1fc6bf1747d5a9bfc4f49f536b7e9e8bad90
Breathe97 <704340916@qq.com>
2025-03-19 11:07:44 +0800

    a

--- src/PrAsyncQueue.ts
+++ src/PrAsyncQueue.ts
@@ -9,6 +9,7 @@ interface PrAsyncQueueItem {
 }
 
 interface Options {
+  key?: string
   timeout?: number
 }
 
@@ -32,13 +33,20 @@ export class PrAsyncQueue {
    */
   add = (func: () => Promise<any>, options: Options = {}) => {
     return new Promise(async (resolve, reject) => {
-      const key = uuid()
+      const _options = { ...this.options, key: uuid(), ...options }
 
-      const timeout = options.timeout || this.options.timeout
+      const { key, timeout } = _options
 
       const item: PrAsyncQueueItem = { key, func, resolve, reject, timeout }
 
+      // 判断是否已经存在该事件
+      {
+        const index = this.queue.findIndex((item) => item.key === key)
+        if (index !== -1) return reject(`the key:${key} is exist.`)
+      }
+
       this.queue.unshift(item)
+
       // console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;', `------->Breathe: promiseQueue add:${key}`, item)
       // 如果当前没有等待事件
       if (!this.activePromise) {
