9a505d782a16c49549e25d4b771566dd635f7ba7
Breathe97 <704340916@qq.com>
2025-07-14 13:33:08 +0800

    add describe

--- src/PrAsyncQueue.ts
+++ src/PrAsyncQueue.ts
@@ -1,14 +1,16 @@
 interface PrAsyncQueueItem {
   key: string
   func: () => Promise<any>
+  describe?: string
+  timeout: number
   resolve: (value: unknown) => any
   reject: (reason?: any) => any
-  timeout: number
 }
 
 interface Options {
   key?: string
   timeout?: number
+  describe?: string
 }
 
 export class PrAsyncQueue {
@@ -32,17 +34,18 @@ export class PrAsyncQueue {
    * @param func 待执行函数
    * @returns 待执行函数结果
    */
-  add = (func: () => Promise<any>, options: Options = {}) => {
+  add = (func: () => Promise<unknown>, options: Options = {}) => {
     return new Promise(async (resolve, reject) => {
       const timeout = options.timeout || this.options.timeout
       const key = options.key || this.#createKey()
+      const describe = options.describe || ''
       // 判断是否已经存在该事件
       {
         const index = this.queue.findIndex((item) => item.key === key)
         if (index !== -1) return reject(`${key} is exist.`)
       }
 
-      const item: PrAsyncQueueItem = { key, func, resolve, reject, timeout }
+      const item: PrAsyncQueueItem = { key, func, describe, timeout, resolve, reject }
       this.queue.unshift(item)
 
       // console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;', `------->Breathe: promiseQueue add:${key}`, item)
@@ -86,7 +89,7 @@ export class PrAsyncQueue {
 
     if (timeout) {
       // 设置超时
-      this.timer = setTimeout(() => {
+      this.timer = window.setTimeout(() => {
         reject(`pr-async-queue: ${key} is timeout.`)
         this.#emitNext() // 再次触发
       }, timeout)
